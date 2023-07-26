4b7b8e60591067f260da5961c5d2bf03d0dcf5ff
Yadnyesh Sawant <yadnyesh2018@gmail.com>
2023-07-26 18:13:09 +0530

    New updates

--- script.js
+++ script.js
@@ -1,69 +1,70 @@
-// script.js
-document.addEventListener("DOMContentLoaded", function () {
-    displayPortfolio();
+// // script.js
+// document.addEventListener("DOMContentLoaded", function () {
+//     displayPortfolio();
 
-    const addStockForm = document.getElementById("addStockForm");
-    addStockForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-        addStockToPortfolio();
-    });
-});
+//     const addStockForm = document.getElementById("addStockForm");
+//     addStockForm.addEventListener("submit", function (event) {
+//         event.preventDefault();
+//         addStockToPortfolio();
+//     });
+// });
 
-function displayPortfolio() {
-    const portfolioTable = document.getElementById("portfolioTable");
-    const portfolioData = JSON.parse(localStorage.getItem("portfolio")) || {};
+// function displayPortfolio() {
+//     const portfolioTable = document.getElementById("portfolioTable");
+//     const portfolioData = JSON.parse(localStorage.getItem("portfolio")) || {};
 
-    portfolioTable.innerHTML = `
-        <tr>
-            <th>Stock Name</th>
-            <th>Quantity</th>
-            <th>Price</th>
-            <th>Real Time Profit/Loss</th>
-        </tr>
-    `;
+//     portfolioTable.innerHTML = `
+//         <tr>
+//             <th>Stock Name</th>
+//             <th>Quantity</th>
+//             <th>Price</th>
+//             <th>Real Time Profit/Loss</th>
+//         </tr>
+//     `;
 
-    for (const stock in portfolioData) {
-        const stockData = portfolioData[stock];
-        const row = `
-            <tr>
-                <td>${stock}</td>
-                <td>${stockData.quantity}</td>
-                <td>${stockData.price}</td>
-                <td><!-- Implement real-time profit/loss calculation here --></td>
-            </tr>
-        `;
-        portfolioTable.insertAdjacentHTML("beforeend", row);
-    }
-}
+//     for (const stock in portfolioData) {
+//         const stockData = portfolioData[stock];
+//         const row = `
+//             <tr>
+//                 <td>${stock}</td>
+//                 <td>${stockData.quantity}</td>
+//                 <td>${stockData.price}</td>
+//                 <td><!-- Implement real-time profit/loss calculation here --></td>
+//             </tr>
+//         `;
+//         portfolioTable.insertAdjacentHTML("beforeend", row);
+//     }
+// }
 
-function addStockToPortfolio() {
-    const stockName = document.getElementById("stock_name").value;
-    const stockQuantity = parseInt(document.getElementById("stock_quantity").value);
-    const stockPrice = parseFloat(document.getElementById("stock_price").value);
+// function addStockToPortfolio() {
+//     const stockName = document.getElementById("stock_name").value;
+//     const stockQuantity = parseInt(document.getElementById("stock_quantity").value);
+//     const stockPrice = parseFloat(document.getElementById("stock_price").value);
 
-    if (!stockName || isNaN(stockQuantity) || isNaN(stockPrice)) {
-        alert("Please fill all the fields with valid values.");
-        return;
-    }
+//     if (!stockName || isNaN(stockQuantity) || isNaN(stockPrice)) {
+//         alert("Please fill all the fields with valid values.");
+//         return;
+//     }
 
-    const portfolioData = JSON.parse(localStorage.getItem("portfolio")) || {};
+//     const portfolioData = JSON.parse(localStorage.getItem("portfolio")) || {};
 
-    if (stockName in portfolioData) {
-        portfolioData[stockName].quantity += stockQuantity;
-    } else {
-        portfolioData[stockName] = {
-            quantity: stockQuantity,
-            price: stockPrice
-        };
-    }
+//     if (stockName in portfolioData) {
+//         portfolioData[stockName].quantity += stockQuantity;
+//     } else {
+//         portfolioData[stockName] = {
+//             quantity: stockQuantity,
+//             price: stockPrice
+//         };
+//     }
 
-    localStorage.setItem("portfolio", JSON.stringify(portfolioData));
+//     localStorage.setItem("portfolio", JSON.stringify(portfolioData));
 
-    displayPortfolio();
+//     displayPortfolio();
 
-    // Clear the form fields after adding to the portfolio
-    document.getElementById("addStockForm").reset();
-}
+//     // Clear the form fields after adding to the portfolio
+//     document.getElementById("addStockForm").reset();
+// }
+
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
     displayPortfolio();
